8f96ded740aea18a4045bb0eca773d32b664bcc3
dzianis.piatrovich <dzianis.piatrovich@gmail.com>
2021-11-02 17:45:51 +0300

    path from src

--- src/main/java/JenkinsTestApp.java
+++ src/main/java/JenkinsTestApp.java
@@ -1,5 +1,21 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class JenkinsTestApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get("src/main/java/text.txt");
+
         System.out.println("Welcome to Jenkins!");
+        System.out.println();
+        try (Scanner scanner = new Scanner(path)) {
+            System.out.println(scanner.nextLine());
+        }  catch (IOException exception) {
+            exception.printStackTrace();
+        }
+
     }
 }
